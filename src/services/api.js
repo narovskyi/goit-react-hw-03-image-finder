146bc517df01f479afedd100a29c29eb605d7794
Oleksii <o.narovskyi@gmail.com>
2023-06-29 15:17:34 +0300

    fix api export

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,11 @@
-export const fetchPhoto = async (page, searchPhrase) => {
+const fetchPhoto = async (page, searchPhrase) => {
     const response = await fetch(`https://pixabay.com/api/?q=${searchPhrase}&page=${page}&key=32074254-ec575441b41af33a027107547&image_type=photo&orientation=horizontal&per_page=12`);
     const result = await response.json();
     return result.hits;
 }
 
-export default { fetchPhoto };
+const api = {
+    fetchPhoto,
+}
+
+export default api;
